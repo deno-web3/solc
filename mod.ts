9357ef2834c3102b7d4f4dc497a25ca46d4eeda7
v1rtl <pilll.pl22@gmail.com>
2023-02-16 13:32:11 +0200

    some more jsdoc

--- mod.ts
+++ mod.ts
@@ -2,4 +2,9 @@ import { wrapper } from './wrapper.ts'
 import { createRequire } from './deps.ts'
 const require = createRequire(import.meta.url)
 
+/**
+ * Load Solidity compiler and wrap it in JS interface
+ * @param soljsonPath compiler file path
+ * @returns Solidity compiler JS interface
+ */
 export const setupSolc = (soljsonPath: string) => wrapper(require(soljsonPath))
